69f168507c7ca12f2408beb60e287b336e7f5d12
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-07-31 00:55:25 +0200

    style - impliment Tailwind styling for the root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Topbar/>
-        <main>
+        <main className="flex flex-row">
           <LeftSidebar/>
-          <section>
-            <div>{children}</div>
+          <section className="main-container">
+            <div className="w-full max-w-4xl">{children}</div>
           </section>
           <RightSidebar/>
         </main>
