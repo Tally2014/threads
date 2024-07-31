e546448accb76de64c1581414f70c5385b7ca6be
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-07-31 02:00:49 +0200

    logic - use next/navigation to highlight the selected LeftSidebar Link

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,18 +1,25 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { usePathname, useRouter } from "next/navigation";
 
 import { sidebarLinks } from "@/constants";
 import { SignedIn, SignOutButton } from '@clerk/nextjs';
 
 export default function LeftSidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
 
   return (
     <section className='custom-scrollbar leftsidebar'>
       <div className='flex w-full flex-1 flex-col gap-6 px-6'>
       {sidebarLinks.map((link) =>{
+        const isActive =
+        (pathname.includes(link.route) && link.route.length > 1) ||
+        pathname === link.route;
         return(
-          <Link className={`leftsidebar_link`}
+          <Link className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
               href={link.route}
               key={link.label}
             >
