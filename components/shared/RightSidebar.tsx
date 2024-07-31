f8afbce9cf07099680e138e2d77097eadf29e83b
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-07-31 02:12:12 +0200

    style - create basic HTML Mockup for the RightSidebar.tsx component

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function RightSidebar() {
   return (
-    <div>RightSidebar</div>
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+      <h3 className='text-heading4-medium text-light-1'>
+          Suggested Communities
+        </h3>
+        <div className='mt-7 flex w-[350px] flex-col gap-9'>
+          Suggested Communites to be mapped here
+        </div>
+      </div>
+      <div className='flex flex-1 flex-col justify-start'>
+      <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
+      <div className='mt-7 flex w-[350px] flex-col gap-10'>
+        Similar Minds to be mapped here
+      </div>
+      </div>
+    </section>
   )
 }
