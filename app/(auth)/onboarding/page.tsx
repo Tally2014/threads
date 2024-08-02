f6d30864776ed1ae8ced0f571163d56459f4257d
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-08-02 02:46:03 +0200

    add - create AccountProfile.tsx and import in onboarding route

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import AccoutProfile from "@/components/forms/AccoutProfile";
 
 export default async function page() {
   return (
     <main>
         <h1 className='head-text'>Onboarding</h1>
+        <AccoutProfile/>
     </main>
   )
 }
---
+++ components/forms/AccoutProfile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AccoutProfile() {
+  return (
+    <div>AccoutProfile</div>
+  )
+}
