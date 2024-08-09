65d84c2ef07867c2b14592a31262b131b7f948e8
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-08-09 20:30:51 +0200

    style - Create AccountProfile Form style using Shadcn

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -9,6 +9,7 @@ import { useForm } from 'react-hook-form';
 import {zodResolver} from '@hookform/resolvers/zod';
 import { UserValidation } from '@/lib/validations/user';
 import * as z from "zod";
+import Image from 'next/image';
 
 interface Props {
   user:{
@@ -43,18 +44,95 @@ export default function AccountProfile({user, btnTitle}: Props) {
       <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col justify-start gap-10'>
         <FormField control={form.control} name="username" render={({field}) => (
           <FormItem>
-            <FormLabel className='text-base-semibold text-light-2'>Username</FormLabel>
-            <FormControl className='flex-1 text-base-semibold text-gray-200'>
-              <Input placeholder='shadcn' {...field}/>
-            </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
+               <FormLabel className='account-form_image-label'>
+                {field.value ? (
+                  <Image
+                    src={field.value}
+                    alt='profile_icon'
+                    width={96}
+                    height={96}
+                    priority
+                    className='rounded-full object-contain'
+                  />
+                ) : (
+                  <Image
+                    src='/assets/profile.svg'
+                    alt='profile_icon'
+                    width={24}
+                    height={24}
+                    className='object-contain'
+                  />
+                )}
+              </FormLabel>
+              <FormControl className='flex-1 text-base-semibold text-gray-200'>
+                <Input
+                  type='file'
+                  accept='image/*'
+                  placeholder='Add profile photo'
+                  className='account-form_image-input'
+                />
+              </FormControl>
           </FormItem>
-        )}>
-          
-        </FormField>
-        <Button type='submit'>Submit</Button>
+        )} />
+
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+               <FormLabel className='text-base-semibold text-light-2'>
+                Name
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type='text'
+                  className='account-form_input no-focus'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}/>
+
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Username
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type='text'
+                  className='account-form_input no-focus'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )} />
+
+        <FormField
+          control={form.control}
+          name='bio'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Bio
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  rows={10}
+                  className='account-form_input no-focus'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}/>
+
+        <Button type='submit' className='bg-primary-500'>{btnTitle}</Button>
       </form>
     </Form>
   )
