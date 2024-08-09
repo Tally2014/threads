e0f61de417f24e6bf5603efa07737f460b27f41d
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-08-09 02:43:31 +0200

    style - Add Shadcn Form in AccountProfile.tsx

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,25 @@
-import AccoutProfile from "@/components/forms/AccoutProfile";
+import AccountProfile from "@/components/forms/AccoutProfile";
 
 export default async function page() {
+
+
+  const userData = {
+    id: "stysvdsbd2233",
+    objectId: "usdnwusdfdfds",
+    username: "Tally",
+    name: "Montso",
+    bio: "Developer",
+    image: "https://i.pinimg.com/236x/db/1f/9a/db1f9a3eaca4758faae5f83947fa807c.jpg",
+  }
   return (
-    <main>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
         <h1 className='head-text'>Onboarding</h1>
-        <AccoutProfile/>
+        <p className='mt-3 text-base-regular text-light-2'>
+        Complete your profile now, to use Threds.
+      </p>
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile user={userData} btnTitle='Continue' />
+      </section>
     </main>
   )
 }
--- components/forms/AccoutProfile.tsx
+++ components/forms/AccoutProfile.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+"use client"
 
-export default function AccoutProfile() {
+import React from 'react'
+import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage,} from '@/components/ui/form';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { useForm } from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import { UserValidation } from '@/lib/validations/user';
+import * as z from "zod";
+
+interface Props {
+  user:{
+    id:string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+export default function AccoutProfile({user, btnTitle}: Props) {
+
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues:{
+      profile_photo:"",
+      name:"",
+      username:"",
+      bio:"",
+    }
+  });
+
+  const onSubmit = async (values: z.infer<typeof UserValidation>) =>{
+    console.log(values);
+  }
+
   return (
-    <div>AccoutProfile</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col justify-start gap-10'>
+        <FormField control={form.control} name="username" render={({field}) => (
+          <FormItem>
+            <FormLabel className='text-base-semibold text-light-2'>Username</FormLabel>
+            <FormControl className='flex-1 text-base-semibold text-gray-200'>
+              <Input placeholder='shadcn' {...field}/>
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+          </FormItem>
+        )}>
+          
+        </FormField>
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
   )
 }
