71d9316e9a8cc0e698eaf78f833e504a5297c19c
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-08-15 19:51:00 +0200

    logic - add default values to AccountProfile fields

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -28,10 +28,10 @@ export default function AccountProfile({user, btnTitle}: Props) {
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues:{
-      profile_photo:"",
-      name:"",
-      username:"",
-      bio:"",
+      profile_photo: user?.image ? user.image : "",
+      name: user?.name ? user.name : "",
+      username: user?.username ? user.username : "",
+      bio: user?.bio ? user.bio : "",
     }
   });
 
@@ -42,7 +42,7 @@ export default function AccountProfile({user, btnTitle}: Props) {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col justify-start gap-10'>
-        <FormField control={form.control} name="username" render={({field}) => (
+        <FormField control={form.control} name="profile_photo" render={({field}) => (
           <FormItem>
                <FormLabel className='account-form_image-label'>
                 {field.value ? (
